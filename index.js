00f5d7941408bbaed87e8065c282f7936a71b5ae
izacpeterson <izacpeterson@gmail.com>
2021-10-04 10:38:10 -0600

    fixed port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
@@ -25,6 +26,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("server listening");
 });
